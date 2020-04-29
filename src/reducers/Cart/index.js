99a13c7e9e4a8edea20d7c021e7ac166c68fb12e
Chrigor <chrigor.edu3@gmail.com>
2020-04-28 21:53:39 -0300

    Adicionando a não repetição de itens no carrinho com immer

--- src/reducers/Cart/index.js
+++ src/reducers/Cart/index.js
@@ -6,7 +6,13 @@ export default function cart(state = INITIAL_STATE, action) {
     switch (action.type) {
         case 'ADD_TO_CART':
             return produce(state, draft => {
-                draft.push({ ... action.data, amount:1});
+                const productIndex = draft.findIndex(product => product.id === action.data.id);
+
+                if (productIndex >= 0) {
+                    draft[productIndex].amount += 1;
+                } else {
+                    draft.push({ ...action.data, amount: 1 });
+                }
             });
         default:
             return state;
